ad298a25f74195579f587cea50b02db59eef950b
r.sato <r.sato@weshare.co.jp>
2024-04-23 13:09:56 +0900

    アーカイブページ　レイアウト微調整

--- index.php
+++ index.php
@@ -4,122 +4,147 @@
     <?php get_template_part('template-parts/header') ?>
     <?php get_template_part('template-parts/contactIcon') ?>
     <?php require_once('template-parts/post_type_info.php'); ?>
-      <main class="l-main p-top">
-        <section class="p-top-googleAd">
-          <div class="p-googleAd-inner"></div>
-        </section>
-        <section class="p-top-slider">
-        <?php
-          $args = array(
-              'posts_per_page' => 6,
-              'post_status' => 'publish',
-              'tax_query' => [
-                  array(
-                      'taxonomy' => 'post_locate',   // カスタムタクソノミーを指定
-                      'field'    => 'slug',       // タームの"slug"または"id"を指定
-                      'terms'    => 'carousel', // 絞り込みたいタームを指定
-                  )
-              ]
-          );
+    <main class="l-main p-top">
+      <section class="p-top-googleAd">
+        <div class="p-googleAd-inner"></div>
+      </section>
+      <section class="p-top-slider">
+      <?php
+        $args = array(
+            'posts_per_page' => 6,
+            'post_status' => 'publish',
+            'tax_query' => [
+                array(
+                    'taxonomy' => 'post_locate',   // カスタムタクソノミーを指定
+                    'field'    => 'slug',       // タームの"slug"または"id"を指定
+                    'terms'    => 'carousel', // 絞り込みたいタームを指定
+                )
+            ]
+        );
 
-          $carousel_query = new WP_Query( $args );
-            if ( $carousel_query->have_posts() ):
-            while ( $carousel_query->have_posts() ):
-                $carousel_query->the_post();
-          ?>
+        $carousel_query = new WP_Query( $args );
+          if ( $carousel_query->have_posts() ):
+          while ( $carousel_query->have_posts() ):
+              $carousel_query->the_post();
+        ?>
 
-          <div class="p-top-slider-item">
-            <a href="" class="p-top-slider-link">
+        <div class="p-top-slider-item">
+          <a href="" class="p-top-slider-link">
+            <?php
+              $thumbnail = get_the_post_thumbnail_url();
+              if (!$thumbnail) {
+                $thumbnail = esc_url(get_template_directory_uri() . '/'). 'assets/images/common/thumbnail-none.jpg';
+              }
+            ?>
+            <img class="p-top-slider-img" src="<?php print $thumbnail; ?>" alt="<?php the_title(); ?>">
+            <div class="p-top-slider-info">
               <?php
-                $thumbnail = get_the_post_thumbnail_url();
-                if (!$thumbnail) {
-                  $thumbnail = esc_url(get_template_directory_uri() . '/'). 'assets/images/common/thumbnail-none.jpg';
-                }
+                $post_type = get_post_type();
               ?>
-              <img class="p-top-slider-img" src="<?php print $thumbnail; ?>" alt="<?php the_title(); ?>">
-              <div class="p-top-slider-info">
-                <?php
-                  $post_type = get_post_type();
-                ?>
-                <span class="p-top-slider-tag" style="<?= get_post_type_info($post_type)['color']; ?>"><?= get_post_type_info($post_type)['name']; ?></span>
-                <h2 class="p-top-slider-title"><?php the_title(); ?></h2>
-              </div>
-            </a>
+              <span class="p-top-slider-tag" style="<?= get_post_type_info($post_type)['color']; ?>"><?= get_post_type_info($post_type)['name']; ?></span>
+              <h2 class="p-top-slider-title"><?php the_title(); ?></h2>
+            </div>
+          </a>
+        </div>
+
+        <?php endwhile;
+          endif;
+          wp_reset_postdata();
+        ?>
+      </section>
+      <section class="p-mid-googleAd">
+        <div class="p-googleAd-inner"></div>
+      </section>
+      <div class="p-mainContent">
+        <section class="p-content">
+          <div class="p-pickUp">
+            <h2 class="p-pickUp-title">Pick Up News</h2>
+            <div class="p-pickUp-list">
+              <?php
+                $args = array(
+                    'posts_per_page' => 4,
+                    'post_status' => 'publish',
+                    'tax_query' => [
+                        array(
+                            'taxonomy' => 'post_locate',   // カスタムタクソノミーを指定
+                            'field'    => 'slug',       // タームの"slug"または"id"を指定
+                            'terms'    => 'pick-up-news', // 絞り込みたいタームを指定
+                        )
+                    ]
+                );
+
+                $carousel_query = new WP_Query( $args );
+                  if ( $carousel_query->have_posts() ):
+                  while ( $carousel_query->have_posts() ):
+                      $carousel_query->the_post();
+              ?>
+              <article class="p-pickUp-article">
+                <a class="p-pickUp-article-link" href="<?php echo get_the_permalink(); ?>">
+                  <?php
+                    $thumbnail = get_the_post_thumbnail_url();
+                    if (!$thumbnail) {
+                      $thumbnail = esc_url(get_template_directory_uri() . '/'). 'assets/images/common/thumbnail-none.jpg';
+                    }
+                  ?>
+                  <img class="p-pickUp-article-thumbnail" src="<?php print $thumbnail; ?>" alt="<?php the_title(); ?>">
+                  <time class="p-pickUp-article-time" datetime="<?= get_the_date('Y.m.d'); ?>"><?= get_the_date('Y.m.d'); ?></time>
+                  <?php
+                    $content = get_the_content();
+                    $content = strip_tags($content);
+                    if (mb_strlen($content) > 40) {
+                        $content = mb_substr($content, 0, 40) . '...'; // 40文字を超える場合は「...」を追加
+                    }
+                  ?>
+                  <p class="p-pickUp-article-text"><?php echo $content; ?></p>
+                </a>
+              </article>
+              
+              <?php
+                endwhile;
+                endif;
+                wp_reset_postdata();
+              ?>
+            </div>
           </div>
+          <div class="p-articles">
+            <?php
+                $article_count = 1;
+                $query = new WP_Query( array(
+                    'post_type' => ['music', 'game', 'animation', 'entertainment'],
+                    'posts_per_page' => 30,
+                    'post_status' => 'publish',
+                ));
 
-          <?php endwhile;
-            endif;
-            wp_reset_postdata();
-          ?>
-        </section>
-        <section class="p-mid-googleAd">
-          <div class="p-googleAd-inner"></div>
-        </section>
-        <div class="p-topContent">
-          <section class="p-content">
-            <div class="p-pickUp">
-              <h2 class="p-pickUp-title">Pick Up News</h2>
-              <div class="p-pickUp-list">
-                <?php
-                  $args = array(
-                      'posts_per_page' => 4,
-                      'post_status' => 'publish',
-                      'tax_query' => [
-                          array(
-                              'taxonomy' => 'post_locate',   // カスタムタクソノミーを指定
-                              'field'    => 'slug',       // タームの"slug"または"id"を指定
-                              'terms'    => 'pick-up-news', // 絞り込みたいタームを指定
-                          )
-                      ]
-                  );
-
-                  $carousel_query = new WP_Query( $args );
-                    if ( $carousel_query->have_posts() ):
-                    while ( $carousel_query->have_posts() ):
-                        $carousel_query->the_post();
-                ?>
-                <article class="p-pickUp-article">
-                  <a class="p-pickUp-article-link" href="<?php echo get_the_permalink(); ?>">
+                  if ( $query->have_posts() ):
+                  while ( $query->have_posts() ):
+                      $query->the_post();
+              ?>
+              <?php if($article_count <= 15): ?>
+                <article class="p-article">
+                  <a class="p-article-link" href="<?php echo get_the_permalink(); ?>">
+                    <time class="p-article-time" datetime="<?= get_the_date('Y.m.d'); ?>"><?= get_the_date('Y.m.d'); ?></time>
                     <?php
                       $thumbnail = get_the_post_thumbnail_url();
                       if (!$thumbnail) {
                         $thumbnail = esc_url(get_template_directory_uri() . '/'). 'assets/images/common/thumbnail-none.jpg';
                       }
                     ?>
-                    <img class="p-pickUp-article-thumbnail" src="<?php print $thumbnail; ?>" alt="<?php the_title(); ?>">
-                    <time class="p-pickUp-article-time" datetime="<?= get_the_date('Y.m.d'); ?>"><?= get_the_date('Y.m.d'); ?></time>
+                    <img src="<?php print $thumbnail; ?>" alt="<?php the_title(); ?>" class="p-article-thumbnail">
+                    <!-- カスタム投稿タイプ　出力 -->
+                    <h2 class="p-article-title"><?php the_title(); ?></h2>
                     <?php
-                      $content = get_the_content();
-                      $content = strip_tags($content);
-                      if (mb_strlen($content) > 40) {
-                          $content = mb_substr($content, 0, 40) . '...'; // 40文字を超える場合は「...」を追加
-                      }
+                      $post_type = get_post_type();
                     ?>
-                    <p class="p-pickUp-article-text"><?php echo $content; ?></p>
+                    <span class="p-article-type" style="<?= get_post_type_info($post_type)['color']; ?>"><?= get_post_type_info($post_type)['name']; ?></span>
+                    <ul class="p-article-tags">
+                      <li class="p-article-tag"></li>
+                    </ul>
                   </a>
                 </article>
-                
-                <?php
-                  endwhile;
-                  endif;
-                  wp_reset_postdata();
-                ?>
-              </div>
-            </div>
-            <div class="p-articles">
-              <?php
-                  $article_count = 1;
-                  $query = new WP_Query( array(
-                      'post_type' => ['music', 'game', 'animation', 'entertainment'],
-                      'posts_per_page' => 30,
-                      'post_status' => 'publish',
-                  ));
-
-                    if ( $query->have_posts() ):
-                    while ( $query->have_posts() ):
-                        $query->the_post();
-                ?>
-                <?php if($article_count <= 15): ?>
+              <?php else: ?>
+                <?php if ($article_count == 16): ?>
+                  <div class="p-article-blind" id="blind-area">
+                <?php endif; ?>
                   <article class="p-article">
                     <a class="p-article-link" href="<?php echo get_the_permalink(); ?>">
                       <time class="p-article-time" datetime="<?= get_the_date('Y.m.d'); ?>"><?= get_the_date('Y.m.d'); ?></time>
@@ -141,62 +166,37 @@
                       </ul>
                     </a>
                   </article>
-                <?php else: ?>
-                  <?php if ($article_count == 16): ?>
-                    <div class="p-article-blind" id="blind-area">
+                  <?php if ($article_count == 30): ?>
+                  </div>
+                  <!-- p-article-blind -->
                   <?php endif; ?>
-                    <article class="p-article">
-                      <a class="p-article-link" href="<?php echo get_the_permalink(); ?>">
-                        <time class="p-article-time" datetime="<?= get_the_date('Y.m.d'); ?>"><?= get_the_date('Y.m.d'); ?></time>
-                        <?php
-                          $thumbnail = get_the_post_thumbnail_url();
-                          if (!$thumbnail) {
-                            $thumbnail = esc_url(get_template_directory_uri() . '/'). 'assets/images/common/thumbnail-none.jpg';
-                          }
-                        ?>
-                        <img src="<?php print $thumbnail; ?>" alt="<?php the_title(); ?>" class="p-article-thumbnail">
-                        <!-- カスタム投稿タイプ　出力 -->
-                        <h2 class="p-article-title"><?php the_title(); ?></h2>
-                        <?php
-                          $post_type = get_post_type();
-                        ?>
-                        <span class="p-article-type" style="<?= get_post_type_info($post_type)['color']; ?>"><?= get_post_type_info($post_type)['name']; ?></span>
-                        <ul class="p-article-tags">
-                          <li class="p-article-tag"></li>
-                        </ul>
-                      </a>
-                    </article>
-                    <?php if ($article_count == 30): ?>
-                    </div>
-                    <!-- p-article-blind -->
-                    <?php endif; ?>
-                <?php endif; ?>
-                <?php
-                  $article_count++;
-                  endwhile;
-                  endif;
-                  wp_reset_postdata();
-                ?>
-              <div class="p-articles-bottom">
-                <button class="p-articles-more" id="add-more">もっと見る</button>
-              </div>
+              <?php endif; ?>
+              <?php
+                $article_count++;
+                endwhile;
+                endif;
+                wp_reset_postdata();
+              ?>
+            <div class="p-articles-bottom">
+              <button class="p-articles-more" id="add-more">もっと見る</button>
             </div>
-          </section>
-          <section class="p-side"></section>
-        </div>
-      </main>
-      <?php get_template_part('template-parts/footer') ?>
-      <script>
-        $('.p-top-slider').slick({
-          dots: true,
-          slidesToShow: 2.325,
-        });
-        $('#add-more').click(function() {
-          $('#blind-area').addClass('is-more-show');
-          $(this).css('display', 'none')
-        });
-        // $('.p-pickUp-list').slick({
-        //   dots: true,
-        //   slidesToShow: 4,
-        // });
-      </script>
+          </div>
+        </section>
+        <section class="p-side"></section>
+      </div>
+    </main>
+    <?php get_template_part('template-parts/footer') ?>
+    <script>
+      $('.p-top-slider').slick({
+        dots: true,
+        slidesToShow: 2.325,
+      });
+      $('#add-more').click(function() {
+        $('#blind-area').addClass('is-more-show');
+        $(this).css('display', 'none')
+      });
+      // $('.p-pickUp-list').slick({
+      //   dots: true,
+      //   slidesToShow: 4,
+      // });
+    </script>
--- template-archives/archive-music.php
+++ template-archives/archive-music.php
@@ -1,80 +1,92 @@
 <?php get_template_part('template-parts/head') ?>
-<?php get_template_part('template-parts/header') ?>
-<?php
-	$post_type = get_post_type();
+<div class="l-wrap">
+    <!-- 閉じタグは_footer.ejs -->
+    <?php get_template_part('template-parts/header') ?>
+    <main class="l-main">
+        <section class="p-top-googleAd">
+            <div class="p-googleAd-inner"></div>
+        </section>
+        <?php
+            $post_type = get_post_type();
 
-    $args = [
-        'breadcrumb_slug_arr' => [],
-        'breadcrumb_arr' => [$post_type]
-    ];
+            $args = [
+                'breadcrumb_slug_arr' => [],
+                'breadcrumb_arr' => [$post_type]
+            ];
 
-    get_template_part('template-parts/breadcrumb', null, $args);
+            get_template_part('template-parts/breadcrumb', null, $args);
 
-    $buttonValue = "all";
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $buttonValue = $_POST['button'];
-    }
-?>
+            $buttonValue = "all";
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $buttonValue = $_POST['button'];
+            }
+        ?>
+        <div class="p-mainContent">
+          <section class="p-content">
+            <form action="" method="post" class="p-article-header">
+                <button class="p-article-tab<?php $tabActive = $buttonValue == 'all' ?  ' is-tabActive' : ''; echo $tabActive; ?>" type="submit" name="button" value="all"><span>A</span>LL</button>
+                <button class="p-article-tab<?php $tabActive = $buttonValue == 'news' ?  ' is-tabActive' : ''; echo $tabActive; ?>" class="p-article-tab" type="submit" name="button" value="news"><span>N</span>EWS</button>
+                <button class="p-article-tab<?php $tabActive = $buttonValue == 'interview' ?  ' is-tabActive' : ''; echo $tabActive; ?>" type="submit" name="button" value="interview"><span>I</span>NTERVIEW</button>
+                <button class="p-article-tab<?php $tabActive = $buttonValue == 'column' ?  ' is-tabActive' : ''; echo $tabActive; ?>" type="submit" name="button" value="column"><span>C</span>OLUMN</button>
+                <button class="p-article-tab<?php $tabActive = $buttonValue == 'report' ?  ' is-tabActive' : ''; echo $tabActive; ?>" type="submit" name="button" value="report"><span>R</span>EPORT</button>
+                <button class="p-article-tab<?php $tabActive = $buttonValue == 'another' ?  ' is-tabActive' : ''; echo $tabActive; ?>" type="submit" name="button" value="another"><span>A</span>NOTHER</button>
+            </form>
+            <?php
+                $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+                $args = [];
+                if ($buttonValue == "all") {
+                    $buttonValue = ['news', 'interview', 'column', 'report', 'another'];
+                }
+                $args = array(
+                    'post_type' => 'music',
+                    'posts_per_page' => 1,
+                    'post_status' => 'publish',
+                    'paged' => $paged,
+                    'tax_query' => [
+                        [
+                            'taxonomy' => 'category',   // カスタムタクソノミーを指定
+                            'field'    => 'slug',       // タームの"slug"または"id"を指定
+                            'terms'    => $buttonValue, // 絞り込みたいタームを指定
+                        ]
+                    ]
+                );
 
-<form action="" method="post" class="p-article-header">
-    <button class="p-article-tab" type="submit" name="button" value="all"><span>A</span>LL</button>
-    <button class="p-article-tab" class="p-article-tab" type="submit" name="button" value="news"><span>N</span>EWS</button>
-    <button class="p-article-tab" type="submit" name="button" value="interview"><span>I</span>NTERVIEW</button>
-    <button class="p-article-tab" type="submit" name="button" value="column"><span>C</span>OLUMN</button>
-    <button class="p-article-tab" type="submit" name="button" value="report"><span>R</span>EPORT</button>
-    <button class="p-article-tab" type="submit" name="button" value="another"><span>A</span>NOTHER</button>
-</form>
-<?php
-    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-    $args = [];
-    if ($buttonValue == "all") {
-        $buttonValue = ['news', 'interview', 'column', 'report', 'another'];
-    }
-    $args = array(
-        'post_type' => 'music',
-        'posts_per_page' => 1,
-        'post_status' => 'publish',
-        'paged' => $paged,
-        'tax_query' => [
-            [
-                'taxonomy' => 'category',   // カスタムタクソノミーを指定
-                'field'    => 'slug',       // タームの"slug"または"id"を指定
-                'terms'    => $buttonValue, // 絞り込みたいタームを指定
-            ]
-        ]
-    );
 
+                $wp_query = new WP_Query( $args );
+                if ( $wp_query->have_posts() ):
+                while ( $wp_query->have_posts() ):
+                    $wp_query->the_post();
+            ?>
 
-    $wp_query = new WP_Query( $args );
-    if ( $wp_query->have_posts() ):
-    while ( $wp_query->have_posts() ):
-        $wp_query->the_post();
-?>
+            <article class="news-item">
+                <a href="<?php the_permalink(); ?>" class="news-item__inner">
+                <div class="news-item__media js-img-bg">
+                    <img src="<?php print $thumbnail; ?>" alt="">
+                </div>
+                <div class="news-item__body">
+                    <h2 class="news-item__title"><?php the_title(); ?></h2>
+                    <time datetime="the_time( 'Y-m-d' )"><?php the_time( 'Y.m.d' ); ?></time>
+                </div>
+                </a>
+            </article>
 
-<article class="news-item">
-    <a href="<?php the_permalink(); ?>" class="news-item__inner">
-    <div class="news-item__media js-img-bg">
-        <img src="<?php print $thumbnail; ?>" alt="">
-    </div>
-    <div class="news-item__body">
-        <h2 class="news-item__title"><?php the_title(); ?></h2>
-        <time datetime="the_time( 'Y-m-d' )"><?php the_time( 'Y.m.d' ); ?></time>
-    </div>
-    </a>
-</article>
-
-<?php
-    endwhile;
-    endif;
-    wp_reset_postdata();
-?>
-<div class="p-pagination">
-<?php echo paginate_links(
-    array (
-        'type' => 'list',
-        'prev_text' => '＜',
-        'next_text' => '＞'
-    ));
-?>
-</div>
-<?php get_template_part('template-parts/footer') ?>
+            <?php
+                endwhile;
+                endif;
+                wp_reset_postdata();
+            ?>
+            <div class="p-pagination">
+                <?php echo paginate_links(
+                    array (
+                        'type' => 'list',
+                        'prev_text' => '＜',
+                        'next_text' => '＞'
+                    ));
+                ?>
+            </div>
+          </section>
+          <section class="p-side"></section>
+        </div>
+        <!-- ./p-mainContent -->
+    </main>
+    <?php get_template_part('template-parts/footer') ?>
