aa235e41cdf875693a7d295eed5361449c908025
wes-rsato <r.sato@weshare.co.jp>
2025-05-13 10:11:46 +0900

    fixed 関連記事 pos

--- Lotus/template-parts/side.php
+++ Lotus/template-parts/side.php
@@ -1,6 +1,6 @@
 <section class="p-side">
   <div class="p-side-inner">
-    <div class="p-side01">
+    <div class="p-side-x">
       <h3>X(twitter)</h3>
       <a class="twitter-timeline" data-height="400" href="https://twitter.com/lotus_magic_d?ref_src=twsrc%5Etfw">Tweets by lotus_magic_d</a>
       <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
